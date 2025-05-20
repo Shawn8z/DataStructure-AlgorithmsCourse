f30082456a0a34b8b510a66807317f9d8b464773
Shawn <lcorel86@gmail.com>
2025-05-20 00:31:10 -0500

    two sample greedy and its edge failed edge cases

--- week3_greedy_algorithms/1_money_change/change.js
+++ week3_greedy_algorithms/1_money_change/change.js
@@ -1,15 +1,18 @@
 // by Alexander Nikolskiy
 
 
-coins = [10, 5, 1];
+boxOfCoint = [10, 5, 1];
 
-function change(input) {
+function change(input, coins) {
+    changes = [...coins.sort((a, b) => b - a)]
+    console.log(changes)
     const returnCoins = [];
     let newTotal = input;
     let coinType = 0;
 
+
     while(newTotal > 0) {
-        const currentCoin = coins[coinType];
+        const currentCoin = changes[coinType];
         const changeCoin = newTotal - currentCoin < 0
 
         if (newTotal === 0) {
@@ -26,12 +29,38 @@ function change(input) {
             newTotal = newTotal - currentCoin;
         }
     }
-    console.log(returnCoins);
+    // console.log(returnCoins, boxOfCoint);
     return returnCoins.length;
 }
 
-const result = change(28);
-console.log(result);
+// console.log(change(28, boxOfCoint));
+console.log(change(8, [1, 4, 6]));
 
+// [2,3,9,2,2]
+// [2, 21]
+// Largest Concatenate
+const input2 = [2, 21];
+function largetsConcatenate(input) {
+    const resultArr = [];
+    const numArr = [...input];
+    const targetLength = input.length;
+    
+    while(resultArr.length !== targetLength) {
+        let currentMax = 0;
+        for (let i = 0; i < targetLength; i += 1) {
+
+            if (numArr[i] < 0) continue;
+            if (currentMax < numArr[i]) currentMax = numArr[i];      
+        }
+        const maxIndex = numArr.findIndex((item) => item === currentMax);
+        console.log(currentMax, '<<<  currentMax')
+        resultArr.push(currentMax); 
+        numArr[maxIndex] = -999;
+    }
+    console.log(resultArr);
+    return resultArr.join('');
+}
+
+// console.log(largetsConcatenate(input2))
 
 
